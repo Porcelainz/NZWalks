c6f654f864714ce1e50a2eb4482e87c8eceffd8b
Porcelain <ai.red.porcelain@gmail.com>
2024-04-08 21:47:05 +0800

    Setting Up identity DbContext and add migration. Complete  Auth Contorller & Register action

---
+++ NZWalksAPI/Data/NZWalksAuthDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace NZWalksAPI.Data
+{
+    public class NZWalksAuthDbContext : IdentityDbContext
+    {
+        public NZWalksAuthDbContext(DbContextOptions<NZWalksAuthDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            var readerRoleId = "b856e27c-84a8-468a-bccb-fdccfcfb4a88";
+            var writerRoleId = "c020e761-0457-4847-9628-b92e1b59f39a";
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole
+                {
+                    Id = readerRoleId,
+                    ConcurrencyStamp = readerRoleId,
+                    Name = "Reader",
+                    NormalizedName = "Reader".ToUpper()
+                },
+                new IdentityRole
+                {
+                    Id = writerRoleId,
+                    ConcurrencyStamp = writerRoleId,
+                    Name = "Writer",
+                    NormalizedName = "Writer".ToUpper()
+                }
+            };
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
