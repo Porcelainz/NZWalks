f7bc64073c6038d91e3e55926fae939af00bf946
Porcelain <ai.red.porcelain@gmail.com>
2024-04-09 21:47:20 +0800

    add image model dto and validate model

---
+++ NZWalksAPI/Controllers/ImagesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using NZWalksAPI.Models.DTO;
+
+namespace NZWalksAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImagesController : ControllerBase
+    {
+        //POST: /api/Images/Upload
+        [HttpPost]
+        [Route("Upload")]
+        public async Task<IActionResult> Upload([FromForm] ImageUploadRequestDto imageUploadRequest)
+        {
+            ValidateFileUpload(imageUploadRequest);
+            if(ModelState.IsValid)
+            {
+                //User repository to upload image
+
+            }
+            return BadRequest(ModelState);
+        }
+
+        private void ValidateFileUpload(ImageUploadRequestDto imageUploadRequest)
+        {
+            var allowExtensions = new string[] { ".jpg", ".jpeg", ".png" };
+            if (!allowExtensions.Contains(Path.GetExtension(imageUploadRequest.File.FileName)))
+            {
+                ModelState.AddModelError("file", "Unsupported file extension");
+            }
+            if (imageUploadRequest.File.Length > 10485760)
+            {
+                ModelState.AddModelError("file", "File size more than 10 MB, plase upload a smaller size file.");
+            }
+        }
+    }
+}
---
+++ NZWalksAPI/Models/DTO/ImageUploadRequestDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalksAPI.Models.DTO
+{
+    public class ImageUploadRequestDto
+    {
+        [Required]
+        public IFormFile File { get; set; }
+        [Required]
+        public string FineName { get; set; }
+        public string? FileDescription { get; set; }
+    }
+}
